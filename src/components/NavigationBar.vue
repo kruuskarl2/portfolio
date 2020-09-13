636dde9e26ae3f10471456b0dbe42e33439641ee
Karl Kruus <karlkruus0@gmail.com>
2020-09-13 19:07:24 +0300

    Add navigation bar component

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="nav-bar"></div>
+</template>
+
+<script>
+export default {
+    name: "NavigationBar"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
